b61898d428e5bdc80e261a36772444bb0280c4d4
Shvetsov Sergey <103686835+SerAlsater@users.noreply.github.com>
2024-04-08 22:49:01 +0300

    message

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["127.0.0.1"],
+    domains: ["127.0.0.1", "api.seralaster.ru"], 
     formats: ["image/avif", "image/webp"],
     },
 };
